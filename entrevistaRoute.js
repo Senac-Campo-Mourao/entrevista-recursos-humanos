8a2e620923652cb38c1c1f6c4e162ff4c4c7283a
Adauto Costa Silva <adauto.0968@aluno.pr.senac.br>
2026-01-04 01:09:15 -0300

    Add files via upload

--- entrevistaRoute.js
+++ entrevistaRoute.js
@@ -1,1 +1,12 @@
-// Arquivo duplicado. Use apenas routes/entrevistaRoute.js para as rotas de entrevista.
+
+const express = require('express');
+const router = express.Router();
+const entrevistaController = require('../controllers/entrevistaController');
+
+router.post('/', entrevistaController.criarEntrevista);
+router.get('/:id', entrevistaController.buscarEntrevistaPorId);
+router.post('/:id/pergunta', entrevistaController.fazerPergunta);
+router.post('/:id/finalizar', entrevistaController.finalizarEntrevista);
+router.get('/:id/resultado', entrevistaController.mostrarResultado);
+
+module.exports = router;
